199c61157f19e67c2c745f68251f53dad3bd0a7f
帕芙 <128116400+Paff-77@users.noreply.github.com>
2024-04-01 03:16:06 +0800

    Update custom_motd_ps1.sh

--- custom_motd_ps1.sh
+++ custom_motd_ps1.sh
@@ -3,6 +3,9 @@
 # 清空 motd 文件
 sudo truncate -s 0 /etc/motd
 
+# 删除 /etc/update-motd.d/ 目录下的所有脚本文件
+sudo rm -f /etc/update-motd.d/*
+
 # 添加新的 motd 内容
 neofetch > /etc/motd
 
@@ -20,5 +23,5 @@ echo "motd and PS1 updated successfully."
 read -rsn 1 -p "按任意键断开 SSH 连接重置..."
 echo
 
-# 断开 SSH
+# 断开 SSH 连接
 kill -SIGHUP $PPID
